88c9c42a69f9899057b3ab4e1f4a5ae6ca3e6ecf
daesoopark <125520878+daesoopark@users.noreply.github.com>
2024-10-01 09:56:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,42 @@
-print('hello world')
+!pip install selenium
+
+from selenium import webdriver
+
+# Chrome 드라이버 다운로드 및 경로 설정(코랩)
+!apt-get update
+!apt install chromium-chromedriver
+!cp /usr/lib/chromium-browser/chromedriver /usr/bin
+
+# Chrome 드라이버 설정
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')  # 화면 표시 없이 실행
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+
+# Chrome 드라이버 초기화
+driver = webdriver.Chrome(options=options)
+
+
+# 웹 페이지 열기
+driver.get("https://select.ridibooks.com/categories/2200?sort=popular&page=1" )
+
+#from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+
+# 페이지의 HTML 가져오기
+html = driver.page_source     # driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+# 원하는 데이터 선택
+bookservices = soup.select('h3.GridBookList_ItemTitle')
+
+# 출력
+for no, book in enumerate(bookservices, 1):
+    print(no, book.text.strip())
+
+# 브라우저 닫기
+#driver.quit()
+
